c6693166936c4eb88bf24ac50d63dee38c39a2fb
milad.bourhani <milad.bourhani@accenture.com>
2019-08-04 23:43:10 +0200

    first commit

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for nerdland
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,123 @@
+module Main where
+
+import Lib
+import Data.Map (Map, fromList, keys, adjustWithKey)
+import System.Random
+import Control.Monad
+
+type Player = String
+
+data Stats = Stats {
+    x     :: Int
+  , y     :: Int
+  , life  :: Int
+  }
+  deriving (Show, Eq)
+
+data World = World {
+    stats :: Map Player Stats
+  }
+  deriving (Show, Eq)
+
+data Action =
+    Move Dir
+  | Swing
+  deriving (Eq, Show)
+
+data Event =
+    PlayerAction Player Action
+  | Rain
+  | Earthquake
+  deriving (Eq, Show)
+
+data Dir = U | D | L | R
+  deriving (Eq, Show)
+
+initialStats = Stats {
+    x = 10
+  , y = 10
+  , life = 10
+  }
+
+initialWorld = World {
+  stats = fromList $ (,) <$> ["milad", "ogre"] <*> [initialStats]
+}
+
+main :: IO ()
+main = loop initialWorld "milad"
+
+loop :: World -> Player -> IO ()
+loop world player = do
+  putStrLn $ show world
+  let players = keys $ stats world
+      actions = fmap (turn player) players
+  events <- sequence $ actions ++ [nature]
+  forM_ events describe
+  let world' = foldl go world events
+  loop world' player
+
+turn :: Player -> Player -> IO (Maybe Event)
+turn player name =
+  if player /= name then
+    npc name
+  else
+    pc name
+
+describe :: Maybe Event -> IO ()
+describe (Just Earthquake) = do
+  putStrLn "Rumble..."
+describe (Just Rain) = do
+  putStrLn "It starts raining."
+describe (Just (PlayerAction player action)) = do
+  putStrLn $ (show player) ++ ": " ++ (show action)
+describe _ = return ()
+
+go :: World -> Maybe Event -> World
+go world (Just (PlayerAction player (Move d))) =
+  world { stats = adjustWithKey (move d) player (stats world) }
+go world (Just (PlayerAction player Swing)) =
+  let opponent = head $ filter (/= player) $ keys $ stats world
+  in world { stats = adjustWithKey swing opponent (stats world) }
+go world _ = world
+
+move :: Dir -> Player -> Stats -> Stats
+move U _ s = s { x = (x s) + 1 }
+move D _ s = s { x = (x s) - 1 }
+move R _ s = s { y = (y s) + 1 }
+move L _ s = s { y = (y s) - 1 }
+
+swing :: Player -> Stats -> Stats
+swing _ s = s { life = (life s) - 1 }
+
+pc :: Player -> IO (Maybe Event)
+pc player = do
+  a <- getLine
+  return $ (PlayerAction player) <$> case a of
+    "up"    -> Just $ Move U
+    "down"  -> Just $ Move D
+    "left"  -> Just $ Move L
+    "right" -> Just $ Move R
+    "swing" -> Just $ Swing
+    _       -> Nothing
+
+npc :: Player -> IO (Maybe Event)
+npc player = do
+  dice <- rollDice
+  return $ (PlayerAction player) <$> case dice of
+    1 -> Just $ Move U
+    2 -> Just $ Move D
+    3 -> Just $ Move L
+    4 -> Just $ Move R
+    5 -> Just Swing
+    _ -> Nothing
+
+nature :: IO (Maybe Event)
+nature = do
+  dice <- rollDice
+  return $ case dice of
+    1 -> Just Rain
+    2 -> Just Earthquake
+    _ -> Nothing
+
+rollDice :: IO Int
+rollDice = getStdRandom (randomR (1, 20))
