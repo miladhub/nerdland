695c4ac77b14936c08c8c3d49827e1c9d3dd6bfd
Milad <milad.bourhani@gmail.com>
2019-12-27 23:19:27 +0100

    read world from file

--- app/Main.hs
+++ app/Main.hs
@@ -4,25 +4,44 @@ import App
 import IoBindings
 import Data.Map (fromList)
 import System.IO
-
-initialStats = Stats {
-    x    = 10
-  , y    = 10
-  , life = 10
-  }
-
-initialWorld = World {
-    player   = "milad"
-  , stats    = fromList $ (,)
-      <$> ["milad", "ogre"]
-      <*> [initialStats]
-  , running  = True
-  }
+import System.Environment (getArgs)
+import Data.Map (Map, fromList)
 
 main :: IO ()
 main = do
   hSetBuffering stdin NoBuffering
   hSetBuffering stdout NoBuffering
   hSetEcho stdin False
-  game initialWorld
+  args <- getArgs
+  world <- readWorld "world.txt"
+  game world
 
+readWorld :: String -> IO World
+readWorld file = do
+  world <- readFile file
+  return $ World
+    {
+      player = readPlayer world
+    , stats = readStats world
+    , running = True
+    }
+
+readPlayer :: String -> String
+readPlayer world =
+  let all = lines world
+      first = words $ all !! 0
+  in first !! 0
+
+readStats :: String -> Map Player Stats
+readStats world =
+  let stats = filter (\l -> length l == 4) $ words <$> lines world
+  in fromList $ readStat <$> stats
+
+readStat :: [String] -> (Player, Stats)
+readStat ws =
+  let player = ws !! 0
+      x      = read $ ws !! 1
+      y      = read $ ws !! 2
+      life   = read $ ws !! 3
+  in (player, Stats x y life)
+  
